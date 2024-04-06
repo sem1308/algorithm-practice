b2cf2d8594dc4607c58620f0ac9f219aec8f0482
sem1308 <43841214+sem1308@users.noreply.github.com>
2024-04-06 23:12:28 +0900

    [Gold V] Title: 개똥벌레, Time: 248 ms, Memory: 27672 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.java
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main{
@@ -16,44 +15,37 @@ public class Main{
         int H = Integer.parseInt(tokens.nextToken());
 
         /*
-            1. 종유석(top) 누적합 구함
-            2. 석순(bottom) 누적합 구함
-            3. top + bottom이 최소가 되는 값과 그 개수 구함
+            1. 종유석 : 중간부터 누적합
+            2. 석순 : 아래부터 중간까지 누적합
+            3. num이 최소가 되는 값과 그 개수 구함
          */
 
-        int[] top = new int[H+1];
-        int[] bottom = new int[H+1];
+        int[] num = new int[H];
 
         boolean isBottom = true;
         for (int i = 0; i < N; i++) {
             int size = Integer.parseInt(br.readLine());
             if(isBottom){
-                bottom[1] += 1;
-                bottom[size+1] -= 1;
+                num[0] += 1;
+                num[size] -= 1;
                 isBottom = false;
             }else{
-                top[H] += 1;
-                top[H-size] -= 1;
+                num[H-size] += 1;
                 isBottom = true;
             }
         }
 
-        for (int i = 2; i <= H; i++) {
-            bottom[i] += bottom[i-1];
-        }
-
-        for (int i = H-1; i >= 1; i--) {
-            top[i] += top[i+1];
+        for (int i = 1; i < H; i++) {
+            num[i] += num[i-1];
         }
 
         int min = Integer.MAX_VALUE;
         int minCnt = 0;
-        for (int i = 1; i <= H; i++) {
-            int num = bottom[i] + top[i];
-            if(min > num){
+        for (int i = 0; i < H; i++) {
+            if(min > num[i]){
                 minCnt = 1;
-                min = num;
-            }else if(min == num){
+                min = num[i];
+            }else if(min == num[i]){
                 minCnt++;
             }
         }
