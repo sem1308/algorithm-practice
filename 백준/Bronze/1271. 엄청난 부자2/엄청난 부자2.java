a03e9d9c4fdeb25475a1acccd2c5e8829427fbf4
sem1308 <43841214+sem1308@users.noreply.github.com>
2024-04-10 23:56:12 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 84 ms, Memory: 11692 KB -BaekjoonHub

---
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main{
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer tokens;
+
+        tokens = new StringTokenizer(br.readLine());
+
+        BigInteger n = new BigInteger(tokens.nextToken());
+        BigInteger m = new BigInteger(tokens.nextToken());
+
+        BigInteger money = n.divide(m);
+        BigInteger change = n.remainder(m);
+
+        System.out.println(money + " " + change);
+    }
+}
