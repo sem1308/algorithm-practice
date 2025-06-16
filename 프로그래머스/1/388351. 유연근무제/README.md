e8f7aadd08272bc02706caeb7210c6670147b846
sem1308 <43841214+sem1308@users.noreply.github.com>
2025-06-16 14:44:43 +0900

    [level 1] Title: 유연근무제, Time: 0.50 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/README.md
+++ 프로그래머스/1/388351. 유연근무제/README.md
@@ -1,10 +1,10 @@
 # [level 1] 유연근무제 - 388351 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388351) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388351?language=java) 
 
 ### 성능 요약
 
-메모리: 80.5 MB, 시간: 1.22 ms
+메모리: 84 MB, 시간: 0.50 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 20일 17:22:32
+2025년 06월 16일 14:44:41
 
 ### 문제 설명
 
--- 프로그래머스/1/388351. 유연근무제/유연근무제.java
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.java
@@ -1,42 +1,46 @@
+import java.util.*;
+
 class Solution {
-    public boolean isWeekend(int day){
-        return day == 6 || day == 7;
-    }
     
-    public int toMinute(int time){
-        return (time / 100) * 60 + time % 100;
+    public int add(int offset, int minute) {
+        int offsethour = offset / 100;
+        int offsetMinute = offset % 100;
+        int sumMinute = offsetMinute + minute;
+        int sum = 0;
+        if(sumMinute >= 60) {
+            sum = offsethour * 100 + (sumMinute + 40);
+        } else {
+            sum = offsethour * 100 + sumMinute;
+        }
+        return sum;
     }
     
     public int solution(int[] schedules, int[][] timelogs, int startday) {
         /*
-            출근 희망 시각 + 10분까지 출근
-            토요일, 일요일 이벤트 X
+            출근 희망 시각 + 10분 이내
+            토,일은 이벤트 X
+            1 : 월 ~ 7: 일
             
-            모든 시각은 "시 * 100 + 분" 으로 표시
-            
-            상품 받을 직원 수 구하기
+            timelog를 보면서 schedule + 10 안에 있으면 이벤트 통과 
         */
         int answer = 0;
         
-        for(int i=0; i < timelogs.length; i++){
-            int[] timelog = timelogs[i];
-            int dueTime = schedules[i];
-            int day = startday - 1;
-
-            boolean shouldGivePresent = true;
-            for(int time : timelog) {
-                if(++day == 8) day = 1;
-                if(isWeekend(day)) continue;
-                
-                int diff = toMinute(time) - toMinute(dueTime);
-                
-                if(diff > 10) {
-                    shouldGivePresent = false;
+        for(int i = 0; i < schedules.length; i++) {
+            int day = startday;
+            int schedule = schedules[i];
+            int limit = add(schedule, 10);
+            boolean isDone = true;
+            for(int timelog : timelogs[i]) {
+                if(day != 6 && day != 7 && timelog > limit) {
+                    isDone = false;
                     break;
                 }
+                day++;
+                if(day == 8) day = 1;
             }
-            
-            if(shouldGivePresent) answer++;
+            if(isDone) {
+                answer++;
+            }
         }
         
         return answer;
